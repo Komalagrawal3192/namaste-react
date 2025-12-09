0e62d6108f077d69eeb5e6c6fb332d5197b71933
Komal <komalagrawal3192@gmail.com>
2025-12-09 09:49:24 +0530

    Laying the Foundation

--- App.js
+++ App.js
@@ -1,22 +1,22 @@
- const heading= React.createElement(
-    "h1",
-     {id: "heading", xyz: "abc"},
-     "Hello World from React!!");
-        const root= ReactDOM.createRoot(document.getElementById("root"));
-        root.render(heading);
+ import React from "react";
+ import ReactDOM from "react-dom/client";
 
-         const parent= React.createElement(
-            "div",
-            {id: "parent"},
-            React.createElement(
-                "div",
-                {id: "child"},[
-                React.createElement(
-                    "h1",{}, "I am an h1 tag"
-                ),
-            React.createElement(
-                    "h2",{}, "I am an h2 tag"
-                )]
-            )
-         );
-         root.render(parent);
+ //React.createElement => ReactElement - JS Object => HTMLElement(render)
+ 
+ const heading = React.createElement(
+    "h1",
+    {id: "heading"},
+    "Namaste React"
+ );
+
+ console.log(heading); 
+
+ //JSX is transpiled before it reaches the JS by Parcel -> Babel
+ //JSX => Babel transpiles it to React.createElement => ReactElement- JSObject => HTMLElement(render)
+
+ const jsxHeading1=(<h1 className="head" tabIndex="5">Namaste React using JSX</h1>);
+
+ const jsxHeading = <h1 id="heading">Rendering JSX</h1>
+ console.log(jsxHeading);
+ const root= ReactDOM.createRoot(document.getElementById("root"));
+ root.render(jsxHeading1);

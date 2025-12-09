bf1069c03a8f412326a0685b004774a346b0af9b
Komal <komalagrawal3192@gmail.com>
2025-12-09 10:17:00 +0530

    Component Composition

--- App.js
+++ App.js
@@ -1,22 +1,31 @@
  import React from "react";
  import ReactDOM from "react-dom/client";
 
- //React.createElement => ReactElement - JS Object => HTMLElement(render)
  
+ //React Element
  const heading = React.createElement(
     "h1",
     {id: "heading"},
     "Namaste React"
  );
 
- console.log(heading); 
+//Functional Component
 
- //JSX is transpiled before it reaches the JS by Parcel -> Babel
- //JSX => Babel transpiles it to React.createElement => ReactElement- JSObject => HTMLElement(render)
+ const Title= () =>(
+    <h1 className="head" tabIndex="5">
+        Namaste React using JSX
+    </h1>
+ );
 
- const jsxHeading1=(<h1 className="head" tabIndex="5">Namaste React using JSX</h1>);
 
- const jsxHeading = <h1 id="heading">Rendering JSX</h1>
- console.log(jsxHeading);
+ //Component Composition
+ const HeadingComponent= ()=>(
+ <div id="container">
+    <Title/>
+    <h1 className="heading">Namaste React Functional Component</h1>
+ </div>);
+
+
  const root= ReactDOM.createRoot(document.getElementById("root"));
- root.render(jsxHeading1);
+ root.render(heading);
+ root.render(<HeadingComponent />);

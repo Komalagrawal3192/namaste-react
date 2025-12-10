703a0fde5e1c40d9e4d6fa3dca2535c6b4e4622a
Komal <komalagrawal3192@gmail.com>
2025-12-10 14:31:51 +0530

    rendering live data

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,43 @@
 import resList from "../utilities/mockData";
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+
 
 const Body = () => {
 
     //Local state variable- Super Powerful variable
     const [ListofRestaurants, setListofRestaurants] = useState(resList);
 
+    useEffect(()=>{
+          fetchData();      
+    },[]);
+
+        const fetchData= async()=>{
+        const swiggyApiUrl ="https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.52110&lng=73.85020&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+   
+        const data= await fetch(`https://proxy.corsfix.com/?${swiggyApiUrl}`);
+        const json = await data.json();
+        console.log(json);
+
+        //Optional Chaining
+        setListofRestaurants(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+   };
+
+   
+
+    if(ListofRestaurants.length === 0)
+        return <Shimmer />;
+
   return (
     <div className="body">
       <div className="filter">
-        <button className="filter-btn" onClick={()=>{
+        <button className="filter-btn" onClick={()=>{   
             const filteredList= ListofRestaurants.filter(
                 (res) => res.info.avgRating > 4.1
             );
             setListofRestaurants(filteredList);
-        }}>
+        }}> 
             Top Rated Components
         </button>
       </div>

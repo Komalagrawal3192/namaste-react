afc961aa2452b354f70031a9dc4ef1021ee1e80b
Komal <komalagrawal3192@gmail.com>
2025-12-17 11:07:47 +0530

    Test framework compatibility tested

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,15 +1,19 @@
-// module.exports = {
-//   testEnvironment: "jsdom",
-//   transform: {
-//     "^.+\\.js$": ["babel-jest", { configFile: "./babel.config.js" }],
-//   },
-//   moduleFileExtensions: ["js", "json"],
-//   roots: ["C:/Users/hp/Desktop/namaste-react/react/src"],
-//   testMatch: ["**/_tests_/**/*.test.js"],
-// };
-
 module.exports = {
   testEnvironment: "jsdom",
   verbose: true,
+  transform: {
+    "^.+\\.js$": ["babel-jest", { configFile: "./babel.config.js" }],
+  },
+  moduleFileExtensions: ["js", "json", "jsx"],
   roots: ["C:/Users/hp/Desktop/namaste-react/react/src"],
+  testMatch: ["**/_tests_/**/*.test.js"],
+  setupFilesAfterEnv: [
+    "<C:/Users/hp/Desktop/namaste-react/react/src/setupTests.js",
+  ],
 };
+
+// module.exports = {
+//   testEnvironment: "jsdom",
+//   verbose: true,
+//   roots: ["C:/Users/hp/Desktop/namaste-react/react/src"],
+// };
